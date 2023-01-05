3e7cf0772a5ef78f18074155cb8d48d9c26fd9bd
anjalidalal <anjalimaria2002@gmail.com>
2023-01-05 12:49:32 +0530

    Cultsport-clone

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import img1 from "../images/img1.jpg";
+import img7 from "../images/img7.jpg";
+import img2 from "../images/img2.jpg";
+import img8 from "../images/img8.jpg";
+import img3 from "../images/img3.webp";
+import img5 from "../images/img5.jpg";
 
 const Carousel = () => {
   return (
     <div className="carousel">
-      <img src={img1} alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+      <div className="slide">
+        <img src={img5} alt="" />
+        <img src={img7} alt="" />
+        <img src={img3} alt="" />
+        <img src={img8} alt="" />
+        <img src={img2} alt="" />
+      </div>
     </div>
   );
 };
