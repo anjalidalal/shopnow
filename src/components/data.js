e1744f79b5cf578ee663fabae8bf77feb81fc4fc
anjalidalal <anjalimaria2002@gmail.com>
2023-01-08 13:22:09 +0530

    Cultsport-clone

--- src/components/data.js
+++ src/components/data.js
@@ -64,6 +64,71 @@ let data = {
       discount: "20% Off",
     },
   ],
+  spinbikes: [
+    {
+      image:
+        "https://images.pexels.com/photos/38296/cycling-bicycle-riding-sport-38296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹11999",
+      off: " ₹40000",
+      discount: "70% Off",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/163491/bike-mountain-mountain-biking-trail-163491.jpeg?auto=compress&cs=tinysrgb&w=600",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹11999",
+      off: " ₹30000",
+      discount: "70% Off",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2883462/pexels-photo-2883462.jpeg?auto=compress&cs=tinysrgb&w=600",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹11000",
+      off: " ₹22000",
+      discount: "40% Off",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3540644/pexels-photo-3540644.jpeg?auto=compress&cs=tinysrgb&w=600",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹12000",
+      off: " ₹24000",
+      discount: "70% Off",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2517951/pexels-photo-2517951.jpeg?auto=compress&cs=tinysrgb&w=600",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹14000",
+      off: " ₹54000",
+      discount: "60% Off",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/1302928/pexels-photo-1302928.jpeg?auto=compress&cs=tinysrgb&w=600",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹16000",
+      off: " ₹34000",
+      discount: "50% Off",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/4906464/pexels-photo-4906464.jpeg?auto=compress&cs=tinysrgb&w=600",
+      brand: "Urban Terrain",
+      content: "UT1000, Black, Steel MTB 27.5 T Shimano Geared Mountain Cycle",
+      price: " ₹19000",
+      off: " ₹30000",
+      discount: "20% Off",
+    },
+  ],
 };
 
 export default data;
