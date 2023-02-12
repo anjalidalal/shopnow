b8ce91e13a959dff76b058ad45506491c9b354cb
anjalidalal <anjalimaria2002@gmail.com>
2023-02-12 11:05:27 +0530

    Styles changed

--- src/components/Redux/Store.js
+++ src/components/Redux/Store.js
@@ -11,10 +11,10 @@ const store = createStore(
   Reducer,
   compose(
     applyMiddleware(thunk),
-    process.env.NODE_ENV === "development"
-      ? window.REDUX_DEVTOOLS_EXTENSION_COMPOSE
-      : null || compose
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // process.env.NODE_ENV === "development"
+    //   ? window.REDUX_DEVTOOLS_EXTENSION_COMPOSE
+    //   : null || compose
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
--- src/components/Routers.jsx
+++ src/components/Routers.jsx
@@ -5,6 +5,7 @@ import AddToCart from "./profile/AddToCart";
 import Login from "./profile/Login";
 import AllProducts from "./subcomponents/AllProducts";
 import PlaceOrder from "../components/order/PlaceOrder";
+import SingleProduct from "./singlePage/SingleProduct";
 
 const Routers = () => {
   return (
@@ -15,6 +16,7 @@ const Routers = () => {
         <Route path="/user-login" element={<Login />} />
         <Route path="/cart" element={<AddToCart />} />{" "}
         <Route path="/orders" element={<PlaceOrder />} />
+        <Route path="/product" element={<SingleProduct />} />
       </Routes>
     </>
   );
